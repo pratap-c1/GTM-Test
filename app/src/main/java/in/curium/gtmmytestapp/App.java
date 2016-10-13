e907644c4860ba25b0056126a0e53f1841f0916a
Pratap Patil <pratap.patil@impressico.com>
2016-10-13 16:56:23 +0530

    updated code for gtm

---
+++ app/src/main/java/in/curium/gtmmytestapp/App.java
@@ -0,0 +1,10 @@
+package in.curium.gtmmytestapp;
+
+import android.app.Application;
+
+public class App extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
--- app/src/main/java/in/curium/gtmmytestapp/MainActivity.java
+++ app/src/main/java/in/curium/gtmmytestapp/MainActivity.java
@@ -1,7 +1,10 @@
 package in.curium.gtmmytestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.google.android.gms.tagmanager.PreviewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    findViewById(R.id.gtm_btn).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, PreviewActivity.class));
+      }
+    });
   }
 }
